d0006632fdb408ec81f129c84f162d0e4324a0a7
Timothy Shields <timothy.shields@live.com>
2015-01-02 15:12:52 -0500

    Some more unit tests for LocalInvoker.

--- CodeMercury/CodeMercury.Tests/LocalInvokerTests.cs
+++ CodeMercury/CodeMercury.Tests/LocalInvokerTests.cs
@@ -6,6 +6,7 @@ using CodeMercury.Services;
 using CodeMercury.Domain.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace CodeMercury.Tests
 {
@@ -33,8 +34,32 @@ namespace CodeMercury.Tests
         public async Task StaticMethodInvocationRunsToCompletion()
         {
             var invocation = InvocationBuilder.Build(() => Math.Max(5, 7));
-            var argument = await LocalInvoker.InvokeAsync(invocation);
-            Assert.AreEqual(7, argument.CastTo<ValueArgument>().Value.CastTo<int>());
+            var result = await LocalInvoker.InvokeAsync(invocation);
+            var value = result.CastTo<ValueArgument>().Value.CastTo<int>();
+            Assert.AreEqual(7, value);
+        }
+
+        private static void CancelableMethod(CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+
+        [TestMethod]
+        public async Task StaticMethodInvocationCancels()
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.Cancel();
+            var invocation = InvocationBuilder.Build(() => CancelableMethod(cancellationTokenSource.Token));
+            var result = await LocalInvoker.InvokeAsync(invocation);
+            Assert.IsInstanceOfType(result, typeof(CanceledArgument));
+        }
+
+        [TestMethod]
+        public async Task StaticMethodInvocationFaults()
+        {
+            var invocation = InvocationBuilder.Build(() => Guid.Parse(null));
+            var result = await LocalInvoker.InvokeAsync(invocation);
+            Assert.IsInstanceOfType(result, typeof(ExceptionArgument));
         }
 
         [TestMethod]
@@ -43,9 +68,10 @@ namespace CodeMercury.Tests
             var serviceId = Guid.NewGuid();
             serviceResolver.Setup(x => x.Resolve(serviceId)).Returns(new List<int> { 3, 5, 7, 6 }).Verifiable();
             var invocation = InvocationBuilder.Build(Argument.Service(serviceId), (List<int> list) => list.IndexOf(7));
-            var argument = await LocalInvoker.InvokeAsync(invocation);
+            var result = await LocalInvoker.InvokeAsync(invocation);
             serviceResolver.Verify(x => x.Resolve(serviceId), Times.Once());
-            Assert.AreEqual(2, argument.CastTo<ValueArgument>().Value.CastTo<int>());
+            var value = result.CastTo<ValueArgument>().Value.CastTo<int>();
+            Assert.AreEqual(2, value);
         }
     }
 }
